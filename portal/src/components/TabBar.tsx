bed92ad961c802570ed8ff892fd782f26f36e682
bmoliveira2025-IT <bmoliveira2025@gmail.com>
2026-01-11 16:56:28 -0300

    Fix: Remove broken motion.nav and restore fixed TabBar

--- portal/src/components/TabBar.tsx
+++ portal/src/components/TabBar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-import { motion } from "framer-motion";
+
 
 function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
